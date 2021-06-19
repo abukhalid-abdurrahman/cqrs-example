ccdaf75e677173add371ad6fd809fbf80f650ff7
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-06-19 15:19:33 +0500

    added handlers for getting order

---
+++ Handlers/Queries/OrderQueries.cs
@@ -0,0 +1,14 @@
+﻿using CQRS_Example.Interfaces.Queries;
+using CQRS_Example.Models.Request;
+using CQRS_Example.Models.Response;
+
+namespace CQRS_Example.Handlers.Queries
+{
+    public class OrderQueries : IOrderQuery
+    {
+        public GetOrderResponseModel GetOrder(GetOrderRequestModel requestModel)
+        {
+            return new GetOrderResponseModel() { OrderId = "73fb5932-e20c-4ce2-baff-486bcb97e880", ProductId = 455488, UserId = 12444 };
+        }
+    }
+}
--- Interfaces/Queries/IOrderQuery.cs
+++ Interfaces/Queries/IOrderQuery.cs
@@ -1,7 +1,10 @@
-﻿namespace CQRS_Example.Interfaces.Queries
+﻿using CQRS_Example.Models.Request;
+using CQRS_Example.Models.Response;
+
+namespace CQRS_Example.Interfaces.Queries
 {
     public interface IOrderQuery
     {
-        
+        GetOrderResponseModel GetOrder(GetOrderRequestModel requestModel);
     }
 }
